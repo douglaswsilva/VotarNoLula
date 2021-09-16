b582a7224e5d32d86b615a142f84ea94a3065d93
Douglas Silva <silvadouglasw@gmail.com>
2021-09-16 00:06:05 -0300

    Initial Commit

---
+++ VotarNoLula/VotarNoLulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VotarNoLulaApp.swift
+//  VotarNoLula
+//
+//  Created by Douglas Silva on 16/09/21.
+//
+
+import SwiftUI
+
+@main
+struct VotarNoLulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
